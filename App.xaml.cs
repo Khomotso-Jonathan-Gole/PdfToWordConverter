976bd6dad717d64df2fa35cca6d1762f1e016265
Khomotso-Jonathan-Gole <khomotsojonathan2@gmail.com>
2025-11-04 17:31:37 +0200

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,21 @@
+/*
+ * PDF to Word Converter
+ * Developed by: Khomotso Jonathan Gole
+ * Contact: 0783672051 / 0795886717
+ * Email: khomotsojonathan2@gmail.com
+ * LinkedIn: https://www.linkedin.com/in/khomotso-jonathan-gole
+ * GitHub: https://github.com/Khomotso-Jonathan-Gole
+ * 
+ * Copyright (c) 2025 Khomotso Jonathan Gole. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+using System.Windows;
+
+namespace PdfToWordConverter
+{
+    public partial class App : Application
+    {
+    }
+}
+
